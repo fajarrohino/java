1fc365c8dd79c8fa93344018a1ff3c68b7e95f25
eannoy <fajarrohino14@gmial.com>
2023-11-19 01:39:54 +0700

    print

---
+++ print and execution flow/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("line 1: hallo guys! ");
+        System.out.println("line 2: hallo!");
+
+        System.out.println("line 3: how are you today?");
+        System.out.println("line 4: fine");
+
+        System.out.printf("line %d", 5);
+        }
+    }
