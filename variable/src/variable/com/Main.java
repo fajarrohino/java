6492237666ce1c85b0c2fec511d28510fa22791e
eannoy <fajarrohino14@gmial.com>
2023-11-19 02:02:16 +0700

    variable, declaration

---
+++ variable/src/variable/com/Main.java
@@ -0,0 +1,18 @@
+package variable.com;
+
+public class Main {
+    public static void main(String[] args){
+        //variable x ( tipe data int)
+        int x = 10; // asignment
+        System.out.println("Data x = " + x);
+
+        // change value x
+        x = 20;
+        System.out.println("New Data x = " + x);
+
+        // Declaration
+        int y;
+        y = 30;
+        System.out.println("Data y = " + y);
+    }
+}
