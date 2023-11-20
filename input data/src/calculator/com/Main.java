07d3392b773115061b6b87fee1b5ecef0aa7cb94
eannoy <fajarrohino14@gmial.com>
2023-11-20 19:15:25 +0700

    calculator

---
+++ input data/src/calculator/com/Main.java
@@ -0,0 +1,37 @@
+package calculator.com;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        float a, b, c;
+        char operator;
+
+        System.out.print("nilai a = ");
+        a = input.nextFloat();
+        System.out.print("operator = ");
+        operator = input.next().charAt(0);
+        System.out.print("nilai b = ");
+        b = input.nextFloat();
+
+        System.out.println("input user = " + a + " " + operator + " " + b);
+
+        if (operator == '+'){
+            c = a + b;
+            System.out.println("hasil = " + c );
+        } else if (operator == '-') {
+            c = a - b;
+            System.out.println("hasil = " + c );
+        } else if (operator == '*') {
+            c = a * b;
+            System.out.println("hasil = " + c );
+        } else if (operator == '/') {
+            c = a / b;
+            System.out.println("hasil = " + c );
+        } else {
+            System.out.println("operator tidak ditemukan!");
+        }
+    }
+}
