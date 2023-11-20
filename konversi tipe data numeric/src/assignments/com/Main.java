93aced44d2d0848cf6637924b0099da539680f82
eannoy <fajarrohino14@gmial.com>
2023-11-20 09:50:43 +0700

    assignments operator

---
+++ konversi tipe data numeric/src/assignments/com/Main.java
@@ -0,0 +1,28 @@
+package assignments.com;
+
+public class Main {
+    public static void main(String[] args){
+        //assignments (=) operator
+        //assignment penjumlahan
+        int a = 10;
+        a += 5;
+        System.out.println("nilai a = 10 + 5 = " + a );
+
+        //asignment penggurangan
+        a -= 8;
+        System.out.println("niali a - 8 = " + a);
+
+        //asignment perkalian
+        int x = 9;
+        x *= 5;
+        System.out.println("nilai x = 9 * 5 = " + x);
+
+        //assignment pembagian
+        x /= 3;
+        System.out.println("nilai x / 3  = " + x);
+
+        //assignment modulus
+        x %= 6;
+        System.out.println("niali x % = " + x);
+    }
+}
